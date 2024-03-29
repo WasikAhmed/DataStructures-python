8dc0e33059859fd1c4baf82b9b2b4ac2ef2786ee
Wasik Ahmed Apon <apon.wasik2001@gmail.com>
2024-03-30 01:00:28 +0600

    Update Linked List. Add new methods.

--- LinkedList/linked_list.py
+++ LinkedList/linked_list.py
@@ -3,6 +3,7 @@ class Node:
         self.data = data
         self.next = None
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -19,12 +20,57 @@ class LinkedList:
             current_node = current_node.next
         current_node.next = new_node
 
+    def prepend(self, data):
+        if not self.head:
+            self.head = Node(data)
+            return
+
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_after_node(self, prev_node, data):
+        if not prev_node.next:
+            prev_node.next = Node(data)
+            return
+
+        new_node = Node(data)
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+
+    def delete_node(self, key):
+        if not self.head:
+            return False
+
+        if self.head.data == key:
+
+            self.head = self.head.next
+            return True
+
+        current_node = self.head
+        while current_node.next:
+            if current_node.next.data == key:
+                current_node.next = current_node.next.next
+                return True
+            current_node = current_node.next
+        return False
+
+    def delete_at_pos(self, pos):
+        return
+
+    def search(self, key):
+        return
+
+    def get_length(self):
+        return
+
+    def reverse(self):
+        return
+
     def print_list(self):
         current_node = self.head
 
-        while current_node.next:
+        while current_node:
             print(current_node.data, end=" -> ")
             current_node = current_node.next
         print("None")
-
-
